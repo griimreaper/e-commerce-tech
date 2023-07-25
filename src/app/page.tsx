f6fbcfec5294488c645179b21d986499190aadd1
Gaston Valles <gastonvallesyeou@gmail.com>
2023-07-24 21:58:59 -0300

    a

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,8 @@ export default function Home() {
             href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
             target="_blank"
             rel="noopener noreferrer"
-          >
+            >
+            <p>AAAAAAAAAAAAAAAAAA</p>
             By{' '}
             <Image
               src="/vercel.svg"
